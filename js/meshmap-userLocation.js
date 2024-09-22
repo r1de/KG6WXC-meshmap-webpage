326b916a029616812be3b5d5579ffe9f54e85e89
Eric - KG6WXC <kg6wxc@gmail.com>
2024-09-22 23:14:59 +0000

    fixing wrong file upload

--- js/meshmap-userLocation.js
+++ js/meshmap-userLocation.js
@@ -6,10 +6,9 @@ function getLocation() {
 	if(navigator.geolocation) {
 		navigator.geolocation.getCurrentPosition(
 			//success!
-//			usePosition,
-			usePosition, null,
-			//error (or nothing, I guess)
-			//do nothing, use center position from config
+			usePosition,
+			//error
+			(err) => console.warn("Geolocation Error ({$err.code}): ${err.message}"),
 			{
 				enableHighAccuracy: true,
 				timeout: 5000,
@@ -20,5 +19,9 @@ function getLocation() {
 }
 
 function usePosition(position) {
-	mapInfo['mapCenterCoords'] = position.coords.latitude;
+//	mapInfo['mapCenterCoords'][0] = position.coords.latitude;
+//	mapInfo['mapCenterCoords'][1] = position.coords.longitude;
+	youAreHereIconRef = L.marker([position.coords.latitude, position.coords.longitude], { icon: userLocationIcon }).addTo(map);
+	youAreHereIconRef._icon.id = "youAreHere";
+	map.panTo([position.coords.latitude, position.coords.longitude]);
 }
